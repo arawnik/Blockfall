76835474b6b6ba7450575e3c6f6d56f8c7010042
arawnik <jere.junttila@outlook.com>
2023-10-24 01:39:32 +0300

    Fix format

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -7,35 +7,35 @@ using Godot;
 /// </summary>
 public partial class MainMenu : CanvasLayer
 {
-    /// <summary>
-    /// Handler for when CampaignButton is pressed.
-    /// </summary>
-    public void OnCampaignButtonPressed()
-    {
-        GetParent<Main>().UpdateAppState(Models.AppState.Campaign);
-    }
+	/// <summary>
+	/// Handler for when CampaignButton is pressed.
+	/// </summary>
+	public void OnCampaignButtonPressed()
+	{
+		GetParent<Main>().UpdateAppState(Models.AppState.Campaign);
+	}
 
-    /// <summary>
-    /// Handler for when LevelButton is pressed.
-    /// </summary>
-    public void OnLevelButtonPressed()
-    {
-        GetParent<Main>().UpdateAppState(Models.AppState.LevelSelect);
-    }
+	/// <summary>
+	/// Handler for when LevelButton is pressed.
+	/// </summary>
+	public void OnLevelButtonPressed()
+	{
+		GetParent<Main>().UpdateAppState(Models.AppState.LevelSelect);
+	}
 
-    /// <summary>
-    /// Handler for when VanillaButton is pressed.
-    /// </summary>
-    public void OnVanillaButtonPressed()
-    {
-        GetParent<Main>().UpdateAppState(Models.AppState.GameVanilla);
-    }
+	/// <summary>
+	/// Handler for when VanillaButton is pressed.
+	/// </summary>
+	public void OnVanillaButtonPressed()
+	{
+		GetParent<Main>().UpdateAppState(Models.AppState.GameVanilla);
+	}
 
-    /// <summary>
-    /// Handler for when IncreasingDifficultyButton is pressed.
-    /// </summary>
-    public void OnIncreasingDifficultyButtonPressed()
-    {
-        GetParent<Main>().UpdateAppState(Models.AppState.GameIncreasingDifficulty);
-    }
+	/// <summary>
+	/// Handler for when IncreasingDifficultyButton is pressed.
+	/// </summary>
+	public void OnIncreasingDifficultyButtonPressed()
+	{
+		GetParent<Main>().UpdateAppState(Models.AppState.GameIncreasingDifficulty);
+	}
 }
